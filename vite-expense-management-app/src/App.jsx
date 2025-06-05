97772003f4fba88fab99229b42e760fd2db3c0c1
sagarboyal <sboyal12@gmail.com>
2025-06-05 14:00:11 +0530

    feat: app.jsx structure changed

--- vite-expense-management-app/src/App.jsx
+++ vite-expense-management-app/src/App.jsx
@@ -1,8 +1,8 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
 import Login from "./components/auth/Login";
 import SignUp from "./components/auth/SignUp";
-import HomePage from "./components/HomePage";
+import HomePage from "./components/Dashboard";
 import { Toaster } from "react-hot-toast";
 import Contact from "./components/common/Contact";
 import About from "./components/common/About";
@@ -10,24 +10,37 @@ import ResetPassword from "./components/auth/ResetPassword";
 import ForgotPassword from "./components/auth/ForgotPassword";
 import NavBar from "./components/common/NavBar";
 import Footer from "./components/common/Footer";
+import Dashboard from "./components/Dashboard";
+import Introduction from "./components/common/Introduction";
 
 function App() {
+  const location = useLocation();
+  // Only render NavBar and Footer if not on login or register page
+  const showPage = !["/login", "/register"].includes(location.pathname);
+
   return (
-    <BrowserRouter>
-      <NavBar />
-      <Toaster position='buttom-center' reverseOrder={false} />
+    <>
+      {showPage && <NavBar />}
+      <Toaster position='bottom-center' reverseOrder={false} />
       <Routes>
         <Route path='/login' element={<Login />} />
-        <Route path='/' element={<HomePage />} />
+        <Route path='/' element={<Introduction />} />
         <Route path='/register' element={<SignUp />} />
         <Route path='/about' element={<About />} />
         <Route path='/contact' element={<Contact />} />
         <Route path='/reset-password' element={<ResetPassword />} />
         <Route path='/forgot-password' element={<ForgotPassword />} />
+        <Route path='/dashboard' element={<Dashboard />} />
       </Routes>
-      <Footer />
-    </BrowserRouter>
+      {showPage && <Footer />}
+    </>
   );
 }
 
-export default App;
+export default function AppWrapper() {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+}
