be38ee49544b743cfa44c2b1077717fccfdd7fb6
sagarboyal <sboyal12@gmail.com>
2025-06-23 20:40:46 +0530

    redsign: updated register page

--- vite-expense-management-app/src/components/auth/SignUp.jsx
+++ vite-expense-management-app/src/components/auth/SignUp.jsx
@@ -65,12 +65,12 @@ const SignUp = () => {
     <div className='flex flex-1 flex-col md:flex-row min-h-screen'>
       {/* Left side */}
       <div className='bg-black text-white flex flex-col justify-center items-start px-20 py-5 md:w-1/2 min-h-screen'>
-        <h1 className='text-2xl lg:text-4xl font-extrabold mb-5 leading-snug max-w-xl'>
+        <h1 className='font-[Poppins] text-3xl font-extrabold lg:text-5xl text-gray-100 mb-4'>
           Manage Enterprise Expenses Effortlessly with{" "}
           <span className='text-red-500'>Trex</span>
         </h1>
 
-        <p className='text-base lg:text-lg text-gray-300 mb-8 max-w-xl'>
+        <p className='font-[Inter] text-base font-medium lg:text-sm text-gray-300 mb-8 max-w-xl'>
           Create your free Trex account and take control of your organization’s
           expenses with real-time tracking, smart analytics, and seamless
           approvals — all in one secure platform.
@@ -80,13 +80,13 @@ const SignUp = () => {
       </div>
 
       {/* Right side */}
-      <div className='flex flex-col md:w-1/2 w-full px-3 py-5'>
+      <div className='flex flex-col md:w-1/2 w-full px-3 py-5 font-[Poppins]'>
         <div className='flex flex-1 items-center justify-center'>
           <form
             className='w-full max-w-sm space-y-6'
             onSubmit={handleSubmit(onSubmitHandler)}
           >
-            <h2 className='text-left text-2xl font-bold text-gray-800'>
+            <h2 className='text-left text-4xl font-bold text-gray-800'>
               Sign up to <span className='text-customRed'>Trex</span>
             </h2>
 
@@ -96,7 +96,7 @@ const SignUp = () => {
                 required
                 id='fullName'
                 type='text'
-                message='*UserName is required'
+                message='Name is required'
                 placeholder='Enter your full name'
                 register={register}
                 errors={errors}
@@ -106,7 +106,7 @@ const SignUp = () => {
                 required
                 id='email'
                 type='email'
-                message='*Email is required'
+                message='Email is required'
                 placeholder='Enter your email'
                 register={register}
                 errors={errors}
@@ -116,7 +116,7 @@ const SignUp = () => {
                 required
                 id='password'
                 type='password'
-                message='*Password is required'
+                message='Password is required'
                 placeholder='Enter your password'
                 register={register}
                 errors={errors}
@@ -136,7 +136,7 @@ const SignUp = () => {
               <span>Already have an account?</span>{" "}
               <a
                 href='/login'
-                className='text-blue-600 font-medium hover:underline hover:text-blue-700 transition duration-200'
+                className='text-black font-medium hover:underline hover:text-silver-500 transition duration-200'
               >
                 Sign in &rarr;
               </a>
