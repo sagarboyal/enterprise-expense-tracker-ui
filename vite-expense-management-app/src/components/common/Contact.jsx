15cc50e7b487f595f2a264770169a586ffb0ad90
sagarboyal <sboyal12@gmail.com>
2025-07-19 14:22:07 +0530

    unused properties removed

--- vite-expense-management-app/src/components/common/Contact.jsx
+++ vite-expense-management-app/src/components/common/Contact.jsx
@@ -256,7 +256,7 @@ const Contact = () => {
               <button
                 type='submit'
                 disabled={isSubmitting}
-                className='inline-flex items-center rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:bg-indigo-400 disabled:cursor-not-allowed'
+                className='inline-flex items-center rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:bg-indigo-400 disabled:cursor-not-allowed'
               >
                 {isSubmitting ? (
                   <>
